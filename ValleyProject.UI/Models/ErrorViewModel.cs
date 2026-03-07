d01925ff9977a1d6328fbaadfe27ae1f8a9f6fd4
Rohit12364 <rohit.k@itdoseinfo.com>
2026-03-07 22:54:17 +0530

    add new project

---
+++ ValleyProject.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ValleyProject.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
